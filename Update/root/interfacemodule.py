8f1cb455425b24f6222e139c74705d9a9628eb08
Dragos L. George <98836566+DragosGeorge@users.noreply.github.com>
2025-04-03 23:32:45 +0300

    Update interfacemodule.py

--- Update/root/interfacemodule.py
+++ Update/root/interfacemodule.py
@@ -17,3 +17,12 @@ Add:
 		if app.QUICK_SELL_SYSTEM:
 			self.wndQuickSell = uiQuickSell.QuickSell()
 			self.wndQuickSell.BindInterface(self)
+
+Search:
+
+		def MarkUnusableInvenSlotOnTopWnd(self
+
+Add:
+
+				elif onTopWnd == player.ON_TOP_WND_QUICK_SELL and self.wndQuickSell and self.wndQuickSell.CantSellItem(InvenSlot):
+					return True
