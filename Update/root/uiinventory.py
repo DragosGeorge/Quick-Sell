c9a3c9bdf6689b9029619fa6ee71b8d566c0dd99
Dragos L. George <98836566+DragosGeorge@users.noreply.github.com>
2025-04-06 19:05:44 +0300

    Update uiinventory.py

--- Update/root/uiinventory.py
+++ Update/root/uiinventory.py
@@ -5,3 +5,25 @@ Add:
 		if self.interface and self.interface.wndQuickSell and self.interface.wndQuickSell.IsShow():
 			chat.AppendChat(chat.CHAT_TYPE_INFO, localeInfo.DONT_USE_ITEM_WHEN_SHOW_CONFIRM)
 			return
+
+Search in class BeltInventoryWindow(ui.ScriptWindow):
+
+        self.wndBeltInventorySlot.RefreshSlot()
+
+Add above:
+
+        if app.QUICK_SELL_SYSTEM:
+            self.RefreshIconSlot()
+
+Add this function:
+
+    if app.QUICK_SELL_SYSTEM:
+        def RefreshIconSlot(self):
+            for k in xrange(item.BELT_INVENTORY_SLOT_COUNT):
+                slotNumber = item.BELT_INVENTORY_SLOT_START + k
+                itemVnum = player.GetItemIndex(slotNumber)
+                if slotNumber in constInfo.QUICK_SELL_ITEMS:
+                    self.wndBeltInventorySlot.SetSlotCoverImage(slotNumber, "inventory/selected_icon.tga")
+                else:
+                   self.wndBeltInventorySlot.EnableSlotCoverImage(slotNumber, False)
+
